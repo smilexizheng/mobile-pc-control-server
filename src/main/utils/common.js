5dfddc88b53e5f53e6f11b0c146dbfdd5f913e00
sayHello <351500405@qq.com>
2025-03-31 17:12:19 +0800

    feat: exclude virtual network

--- src/main/utils/common.js
+++ src/main/utils/common.js
@@ -7,17 +7,38 @@ const os = require('os');
 function getLocalIPs() {
   const interfaces = os.networkInterfaces();
   const ips = [];
+  const platform = process.platform;
+
+  // 定义各平台虚拟接口的正则表达式
+  const virtualPatterns = {
+    linux: /^(docker\d*|br-|virbr\d*|vmnet\d*|veth|lo|tun\d*|tap\d*|ppp\d*|vboxnet\d*|ovs.*|gre.*|gretap.*|ip_vti.*|ip6_vti.*|ip6_gre.*|ip6_gretap.*|ip6_vti64.*)/i,
+    darwin: /^(utun\d*|vmnet\d*|vboxnet\d*|lo0|bridge\d*|enx.*|veth.*|docker.*|cali.*|tun.*|tap.*|ppp.*|ipsec.*|gre.*|gretap.*|ip_vti.*|ip6_vti.*|ip6_gre.*|ip6_gretap.*|ip6_vti64.*)/i,
+    win32: /(VirtualBox|Hyper-V|VMware|Teredo|Microsoft|Loopback|VBox|Nat|NatNetwork|InternalNetwork|PrivateNetwork|PublicNetwork|vEthernet|HyperV|DockerNAT|Docker|WSL|VPN|IPSec|L2TP|PPTP|OpenVPN|TAP-Windows|TunTap|VxLAN|GRE|Geneve|NVGRE|IPSec|L2TP|PPTP|OpenVPN|TAP-Windows|TunTap|VxLAN|GRE|Geneve|NVGRE)/i
+  };
+
+  // 获取当前平台的匹配规则，默认不匹配任何
+  const virtualRegex = virtualPatterns[platform] || /^$/;
+
   for (const interfaceName in interfaces) {
+    // 跳过虚拟接口
+    if (virtualRegex.test(interfaceName)) {
+      continue;
+    }
+
     const addresses = interfaces[interfaceName];
     for (const address of addresses) {
-      // 兼容Node.js版本差异：family可能是'IPv4'或4
-      const family = address.family.replace(/^IPv4$/, '4'); // 确保转换为字符串
-      if (family === '4' && !address.internal) {
+      // 检查是否为IPv4且非内部地址
+      const isIPv4 = String(address.family).toLowerCase() === 'ipv4';
+      if (isIPv4 && !address.internal) {
+        // console.log(`Adding IP: ${address.address}`);
         ips.push(address.address);
+      } else {
+        // console.log(`Skipping address: ${address.address} (not IPv4 or internal)`);
       }
     }
   }
-  return ips;
+
+  return [...new Set(ips)];
 }
 
 let icon = null;
